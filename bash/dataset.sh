8aad9b47181e8e681e461d6d3ad4d7b4ae674baa
Nikicoraz <nicolacorato05@gmail.com>
2026-04-12 16:02:27 +0200

    Cambio di dataset a Github top repositories

--- bash/dataset.sh
+++ bash/dataset.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-DATASET_URL="https://www.kaggle.com/api/v1/datasets/download/asifxzaman/developer-burnout-prediction-dataset7000-samples"
+DATASET_URL="https://www.kaggle.com/api/v1/datasets/download/nalisha/github-top-repositories-dataset-starred-project"
 DATA_DIR="../data"
-ZIP_FILE="$DATA_DIR/developer_burnout_dataset_7000.zip"
+ZIP_FILE="$DATA_DIR/github_top_repositories.zip"
 CSV_FILE="${ZIP_FILE//.zip/.csv}"
 
-LOW_BURNOUT_FILE="$DATA_DIR/burnouts/low_burnout.csv"
-MEDIUM_BURNOUT_FILE="${LOW_BURNOUT_FILE//low/medium}"
-HIGH_BURNOUT_FILE="${LOW_BURNOUT_FILE//low/high}"
+DOMAINS_DIR="$DATA_DIR/domains"
 
 function download_dataset() {
     if ! [ -d "$DATA_DIR" ]; then
@@ -28,40 +26,27 @@ function download_dataset() {
 }
 
 function prepare_dataset() {
-    if ! [ -d "$(dirname "$LOW_BURNOUT_FILE")" ]; then
-        mkdir "$(dirname "$LOW_BURNOUT_FILE")"
-    else
-        if [ -f "$LOW_BURNOUT_FILE" ] && [ -f "$MEDIUM_BURNOUT_FILE" ] && [ -f "$HIGH_BURNOUT_FILE" ]; then
-            echo "Dataset already prepared, using cached values"
-            return 0
-        fi
+    if ! [ -d "$DOMAINS_DIR" ]; then
+        mkdir "$DOMAINS_DIR"
     fi
 
-    # Create the burnout files, and truncate them if they exist
-    truncate -s 0 "$LOW_BURNOUT_FILE"
-    truncate -s 0 "$MEDIUM_BURNOUT_FILE"
-    truncate -s 0 "$HIGH_BURNOUT_FILE"
-
-    # Add csv header without the burnout level to the files
-    header="$(head -n 1 "$CSV_FILE" | cut -d "," -f 1-11)"
-    echo "$header" > "$LOW_BURNOUT_FILE"
-    echo "$header" > "$MEDIUM_BURNOUT_FILE"
-    echo "$header" > "$HIGH_BURNOUT_FILE"
+    # Add csv header without the domain to the files
+    header="$(head -n 1 "$CSV_FILE" | cut -d "," -f 2-)"
 
     progress=0
     # Number of lines without counting the csv header
     total=$(( "$(wc -l "$CSV_FILE" | cut -d " " -f 1)" - 1 ))
     while read -r line
     do
-        burnout="$(echo "$line" | cut -d "," -f 12)"
-        line="$(echo "$line" | cut -d "," -f 1-11)"
-        if [ "$burnout" == "Low" ]; then
-            echo "$line" >> "$LOW_BURNOUT_FILE"
-        elif [ "$burnout" == "Medium" ]; then
-            echo "$line" >> "$MEDIUM_BURNOUT_FILE"
-        elif [ "$burnout" == "High" ]; then
-            echo "$line" >> "$HIGH_BURNOUT_FILE"
+        domain="$(echo "$line" | cut -d "," -f 1)"
+        line="$(echo "$line" | cut -d "," -f 2-)"
+        file="$DOMAINS_DIR/$domain.csv"
+        if ! [ -f "$file" ]; then
+            touch "$file"
+            echo "$header" >> "$file"
         fi
+        echo "$line" >> "$file"
+
         progress=$(( progress + 1 ))
 
         # Print progress bar
